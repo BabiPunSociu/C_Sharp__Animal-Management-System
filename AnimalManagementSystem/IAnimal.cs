4f83adb2aed791ed4540b28ed23404d90ff8fc59
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 15:00:35 +0700

    DungNguyen

---
+++ AnimalManagementSystem/IAnimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalManagementSystem
+{
+    internal interface IAnimal
+    {
+        protected int ID { get; set; }
+        protected string Name { get; set; }
+        protected int Age { get; set; }
+    }
+}
